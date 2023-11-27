702439be8b7d40d9509a527bbc4db7c572b89d05
Jessica <wyx01263@gmail.com>
2023-11-26 23:35:23 -0500

    Set up frontend

---
+++ library-app-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>this is our application</h1>
+    </div>
+  );
+}
+
+export default App;
